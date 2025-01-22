21ab15026ec689624ba34bd2a5271f281d2d8bfe
Samantha L A Silva <samanthaathayde@hotmail.com>
2025-01-21 22:39:48 -0300

    initial commit

---
+++ src/main/java/com/github/cylonsam/ReceitasBackApplication.kt
@@ -0,0 +1,12 @@
+package com.github.cylonsam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class ReceitasBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReceitasBackApplication>(*args)
+}
